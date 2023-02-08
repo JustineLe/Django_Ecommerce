d0043e3fa61151401d58bdd40c622b276a0b539a
JustineLe <nghiale1004@gmail.com>
2023-02-08 15:04:57 +0700

    Create detail page

--- Django_Ecommerce/urls.py
+++ Django_Ecommerce/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from core.views import frontpage
+from core.views import frontpage, shop
+from product.views import product
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', frontpage, name='frontpage')
+    path('', frontpage, name='frontpage'),
+    path('shop/', shop, name='shop'),
+    path('product/', product, name='product'),
 ]
