5f8b814db5ec7920b146dfe9a45e8cf8d81a8c6a
JustineLe <nghiale1004@gmail.com>
2023-02-17 14:06:39 +0700

    Create product list view and detail view

--- product/views.py
+++ product/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from product.models import Product
 
 
 # Create your views here.
-def product(request):
-    return render(request, 'product/product.html')
+def product(request, slug):
+    products = get_object_or_404(Product, slug=slug)
+
+    context = {
+        "products": products
+    }
+
+    return render(request, 'product/product.html', context)
